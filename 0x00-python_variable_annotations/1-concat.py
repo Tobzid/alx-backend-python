f1597144debc9008666b1e80002f63f64656068b
Tobzid <tobzid2013@gmail.com>
2023-04-30 02:12:47 -0700

    0x00-python_variable_annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""type-annotated function concat that takes a string str1 and a string str2  and returns a concatenated string"""
+
+def concat(str1: str, str2: str) -> str:
+    """ concatenated two string"""
+    return str1 + str2
